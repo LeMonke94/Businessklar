3388d42cfa00b435732b9ca29b3f17fa3d393aa5
LeMonke <shresthaalex365@gmail.com>
2026-05-14 00:11:54 +0200

    feat: env import deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import { env } from '@/config/env';
-
-
 function HomePage() {
     return (
         <main>
             <h1>BusinessKlar</h1>
             <p>Coming soon...</p>
-            <p>Supabase URL: {env.NEXT_PUBLIC_SUPABASE_URL}</p>
         </main>
     );
 }
